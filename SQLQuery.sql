b9bc509f7158bfde913a64fa41f1542a89c63f3a
gnabaev <gnabaev@gmail.com>
2024-01-07 22:35:29 +0300

    Написан SQL запрос.

---
+++ SQLQuery.sql
@@ -0,0 +1,3 @@
+﻿SELECT Products.Name AS Product, Categories.Name AS Category FROM Products
+JOIN ProductsCategories ON ProductsCategories.ProductId = Products.Id
+LEFT JOIN Categories ON Categories.Id = ProductsCategories.CategoryId
